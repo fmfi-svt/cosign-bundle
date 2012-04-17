58f9d494b50e47fddc4661ae1e0f7c64f583900f
Martin Sucha <anty.sk@gmail.com>
2012-04-17 11:46:52 +0200

    Pridane logovanie do user providera a logout handlera.
    original-commit-id: c3fe709c5f66d0a41c65a5693e6ca858bad51060

--- src/SVT/CosignBundle/Security/CosignLogoutHandler.php
+++ src/SVT/CosignBundle/Security/CosignLogoutHandler.php
@@ -17,18 +17,28 @@ use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Log\LoggerInterface;
 
 class CosignLogoutHandler implements LogoutHandlerInterface
 {
-    public function __construct()
+    private $logger;
+
+    public function __construct(LoggerInterface $logger = null)
     {
-        
+        $this->logger = $logger;
     }
 
     public function logout(Request $request, Response $response, TokenInterface $token)
     {
+        if ($this->logger) {
+            $this->logger->info('Cosign logout handler invoked');
+        }
         if ($request->server->has('COSIGN_SERVICE')) {
-            $response->headers->clearCookie($request->server->get('COSIGN_SERVICE'), '/');
+            $cookieName = $request->server->get('COSIGN_SERVICE');
+            if ($this->logger) {
+                $this->logger->info(sprintf('Clearing %s cookie', $cookieName));
+            }
+            $response->headers->clearCookie($cookieName);
         }
     }
     
