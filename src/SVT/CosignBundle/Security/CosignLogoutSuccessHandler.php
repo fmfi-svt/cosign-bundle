b497ee93ec648be23eddb0d4cf70307392816bec
Tomi Belan <tomi.belan@gmail.com>
2012-04-21 23:53:35 +0200

    do logout configu netreba pisat URL instancie, staci vseobecny cosign prefix.
    original-commit-id: dd86caef08171687d47252b5700e950211d792de

--- src/SVT/CosignBundle/Security/CosignLogoutSuccessHandler.php
+++ src/SVT/CosignBundle/Security/CosignLogoutSuccessHandler.php
@@ -2,7 +2,7 @@
 /**
  * This file contains Cosign logout handler.
  *
- * @copyright Copyright (c) 2011 The FMFI Anketa authors (see AUTHORS).
+ * @copyright Copyright (c) 2011-2012 The FMFI Anketa authors (see AUTHORS).
  * Use of this source code is governed by a license that can be
  * found in the LICENSE file in the project root directory.
  *
@@ -13,27 +13,35 @@
 
 namespace SVT\CosignBundle\Security;
 
-use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+use Symfony\Component\Security\Http\HttpUtils;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Cookie;
 
-class CosignLogoutHandler implements LogoutHandlerInterface
+class CosignLogoutSuccessHandler implements LogoutSuccessHandlerInterface
 {
+    private $httpUtils;
+    private $cosignLogoutPrefix;
     private $logger;
 
-    public function __construct(LoggerInterface $logger = null)
+    public function __construct(HttpUtils $httpUtils, $cosignLogoutPrefix, LoggerInterface $logger = null)
     {
+        $this->httpUtils = $httpUtils;
+        $this->cosignLogoutPrefix = $cosignLogoutPrefix;
         $this->logger = $logger;
     }
 
-    public function logout(Request $request, Response $response, TokenInterface $token)
+    public function onLogoutSuccess(Request $request)
     {
         if ($this->logger) {
-            $this->logger->info('Cosign logout handler invoked');
+            $this->logger->info('Cosign logout success handler invoked');
         }
+        
+        $response = $this->httpUtils->createRedirectResponse($request,
+            $this->cosignLogoutPrefix . $request->getUriForPath('/'));
+        
         if ($request->server->has('COSIGN_SERVICE')) {
             $cookieName = $request->server->get('COSIGN_SERVICE');
             if ($this->logger) {
@@ -42,6 +50,8 @@ class CosignLogoutHandler implements LogoutHandlerInterface
             // We need to use secure = true to correctly clear cookie
             $response->headers->setCookie(new Cookie($cookieName, null, 1, '/', null, true));
         }
+        
+        return $response;
     }
     
 }
